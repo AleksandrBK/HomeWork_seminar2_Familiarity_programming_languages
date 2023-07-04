77448563b71244eacefa5eef67a65cd49d6f2e5a
AleksandrBK <lebedevavlv@gmail.com>
2023-07-04 08:23:14 +0400

    task 1

---
+++ task1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 100 || number >= 1000)
+{
+    Console.WriteLine($"Вы ввели не трехзначное число >{number}< пожалуйста повторите попытку");
+    return;
+}
+
+int secondDigit = (number % 100) / 10;
+
+Console.WriteLine($"Вторая цифра введенного числа: {secondDigit}");
