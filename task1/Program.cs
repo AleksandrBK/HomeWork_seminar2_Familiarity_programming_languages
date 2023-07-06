72b53cb54bf6d4cf1a0cfd29f0ea06a572b370d9
AleksandrBK <lebedevavlv@gmail.com>
2023-07-06 06:48:23 +0400

    task 1

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,12 +1,50 @@
-﻿Console.Write("Введите трехзначное число: ");
-int number = Convert.ToInt32(Console.ReadLine());
+﻿// int number;
+// int secondDigit;
 
-if (number < 100 || number >= 1000)
+// while (true)
+// {
+//     Console.Write("Введите трехзначное число: ");
+//     number = Convert.ToInt32(Console.ReadLine());
+
+//     if (number >= 100 && number < 1000)
+//     {
+//         secondDigit = (number % 100) / 10;
+//         break; 
+//     }
+
+//     Console.WriteLine($"Вы ввели не трехзначное число >{number}<, пожалуйста, повторите попытку");
+// }
+
+// Console.WriteLine($"Вторая цифра введенного числа: {secondDigit}");
+
+int number;
+int secondDigit;
+
+while (true)
 {
-    Console.WriteLine($"Вы ввели не трехзначное число >{number}< пожалуйста повторите попытку");
-    return;
-}
+    Console.Write("Введите трехзначное число: ");
+    string input = Console.ReadLine();
 
-int secondDigit = (number % 100) / 10;
+    if (string.IsNullOrEmpty(input))
+    {
+        Console.WriteLine("Вы не ввели число, пожалуйста повторите попытку");
+        continue; 
+    }
+
+    if (!int.TryParse(input, out number))
+    {
+        Console.WriteLine("Вы ввели некорректное значение, пожалуйста повторите попытку");
+        continue; 
+    }
+
+    if (number < 100 || number >= 1000)
+    {
+        Console.WriteLine($"Вы ввели не трехзначное число >{number}<, пожалуйста повторите попытку");
+        continue; 
+    }
+
+    secondDigit = (number % 100) / 10;
+    break; 
+}
 
 Console.WriteLine($"Вторая цифра введенного числа: {secondDigit}");
